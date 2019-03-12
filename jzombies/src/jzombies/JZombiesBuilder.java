3d48c8a8c9ee33b2fe9685acfb592775ba938e63
Louxi3152 <34438507+Louxi3152@users.noreply.github.com>
2019-03-12 12:03:18 +0100

    go for Java Zombie

--- jzombies/src/jzombies/JZombiesBuilder.java
+++ jzombies/src/jzombies/JZombiesBuilder.java
@@ -3,11 +3,13 @@ package jzombies;
 import repast.simphony.context.Context;
 import repast.simphony.context.space.continuous.ContinuousSpaceFactory;
 import repast.simphony.context.space.continuous.ContinuousSpaceFactoryFinder;
+import repast.simphony.context.space.graph.NetworkBuilder;
 import repast.simphony.context.space.grid.GridFactory;
 import repast.simphony.context.space.grid.GridFactoryFinder;
 import repast.simphony.dataLoader.ContextBuilder;
 import repast.simphony.random.RandomHelper;
 import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.NdPoint;
 import repast.simphony.space.continuous.RandomCartesianAdder;
 import repast.simphony.space.grid.Grid;
 import repast.simphony.space.grid.GridBuilderParameters;
@@ -18,6 +20,10 @@ public class JZombiesBuilder implements ContextBuilder<Object> {
 
 	@Override
 	public Context build(Context<Object> context) {
+		
+		NetworkBuilder<Object> netBuilder = new NetworkBuilder<Object>("infection network", context, true);
+		netBuilder.buildNetwork();
+		
 		context.setId("jzombies");
 
 		ContinuousSpaceFactory spaceFactory = ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null);
@@ -37,6 +43,11 @@ public class JZombiesBuilder implements ContextBuilder<Object> {
 		for (int i = 0; i < humanCount; i++) {
 			int energy = RandomHelper.nextIntFromTo(4, 10);
 			context.add(new Human(space, grid, energy));
+		}
+
+		for (Object obj : context) {
+			NdPoint pt = space.getLocation(obj);
+			grid.moveTo(obj, (int) pt.getX(), (int) pt.getY());
 		}
 
 		return context;
